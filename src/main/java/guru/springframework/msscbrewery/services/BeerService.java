a524de5eff8410a20fb21f8930a25866a73b1775
fatih gunes <fatih.gunes@yahoo.com>
2020-10-17 22:48:08 +0300

    PutMapping added

--- src/main/java/guru/springframework/msscbrewery/services/BeerService.java
+++ src/main/java/guru/springframework/msscbrewery/services/BeerService.java
@@ -11,4 +11,6 @@ public interface BeerService {
     BeerDto getBeerById(UUID beerId);
 
     BeerDto SaveNewBeer(BeerDto beerDto);
+
+    void updateBeer(UUID beerId, BeerDto beerDto);
 }
--- src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
@@ -29,7 +29,7 @@ public class BeerController {
     }
 
     @PostMapping
-    public ResponseEntity handlePost(BeerDto beerDto) {
+    public ResponseEntity handlePost(@RequestBody BeerDto beerDto) {
 
         BeerDto savedDto = beerService.SaveNewBeer(beerDto);
 
@@ -39,4 +39,12 @@ public class BeerController {
         return new ResponseEntity(headers, HttpStatus.CREATED);
     }
 
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity handleUpdate(@PathVariable UUID beerId, @RequestBody BeerDto beerDto) {
+
+        beerService.updateBeer(beerId, beerDto);
+
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
 }
